b4d1a317559f2f06e8ed62a95e7d456f1c9deeba
alpinnio <107537249+alpinnio@users.noreply.github.com>
2022-07-31 15:48:54 +0700

    Update mess.js

--- src/mess.js
+++ src/mess.js
@@ -1,18 +1,18 @@
 module.exports.mess = {
-ban: 'Maaf kakak telah di banned',
-limit: 'Limit kamu sudah habis\nMainkan game untuk mendapatkan limit!',
-wait: 'Proses kak',
-eror: 'Maaf terjadi kesalahan !!',
+ban: 'Lo udah di banned tolol',
+limit: 'Limit lo udah abis goblok\nMainin game biar dapet limit ya kontol!',
+wait: 'Bentar Ngab, Ga Sabaran Gw Satirin Lu',
+eror: 'Erorr Cok Kontol Lah Ah Memek !!',
 success: 'Sukses️',
 error: {
-stick: 'Itu bukan sticker kak !!',
+stick: 'Itu bukan sticker anying !!',
 Iv: 'Link invalid !!'
 },
 only: {
 prem: 'Fitur khusus member premium !!\nBeli premium untuk membuka fitur premium untuk melihat list harga ketik #sewapremium',
-group: 'Fitur khusus grup !!',
-owner: 'Fitur khusus owner !!',
-admin: 'Fitur khusus admin !!',
-Badmin: 'Silakan jadikan bot admin dulu !!'
+group: 'Fitur khusus grup goblok !!',
+owner: 'Lo Bukan Owner Kontol !!',
+admin: 'Fitur Ini Cuman Bisa Dipake Admin Tololll !!',
+Badmin: 'Jadiin Bot Admin Dlu Lah Anj !!'
 }
 }
